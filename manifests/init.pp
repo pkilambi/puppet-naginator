cdfa6115214a7bd1c109e15addd32d192db134e6
Pradeep Kilambi <pkilambi@cisco.com>
2013-05-21 15:27:23 -0400

    Add rhel support for naginator

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,66 +8,93 @@
 #
 
 class naginator {
+  case $::osfamily {
+   'redhat': {
+        $service_name      = 'nagios'
+	$package_name      = 'nagios'
+	$package_name_list = [ "nagios", "nagios-plugins-nrpe", "nagios-plugins", ]
+        $cfg_files         = [ "/etc/nagios/conf.d/nagios_command.cfg",
+                               "/etc/nagios/conf.d/nagios_host.cfg",
+                               "/etc/nagios/conf.d/nagios_hostextinfo.cfg",
+                               "/etc/nagios/conf.d/nagios_service.cfg", ]
+        $nagios_users      = '/etc/nagios/htpasswd.users'
+        $cgi_cfg           = '/etc/nagios/cgi.cfg'
+   }
+   'debian': {
+        $service_name      = 'nagios3'
+        $package_name      = 'nagios3'
+        $package_name_list = [ "nagios3", "nagios-nrpe-plugin", "nagios-plugins",
+                               "nagios3-doc", ]
+        $cfg_files         = [ "/etc/nagios3/conf.d/nagios_command.cfg",
+                               "/etc/nagios3/conf.d/nagios_host.cfg",
+                               "/etc/nagios3/conf.d/nagios_hostextinfo.cfg",
+                               "/etc/nagios3/conf.d/nagios_service.cfg", ]
+        $nagios_users      = "/etc/nagios3/htpasswd.users"
+        $cgi_cfg           = '/etc/nagios3/cgi.cfg'
+   }
+    default: {
+        fail("unsupported osfamily: $::osfamily")
+   }
+ }
 
-    package { [ "nagios3", "nagios-nrpe-plugin", "nagios-plugins", "nagios3-doc", ]:
+    package { $package_name_list:
         ensure => installed,
     }
 
-    service { "nagios3":
+    service { $naginator::service_name:
         ensure     => running,
         enable     => true,
         hasstatus  => true,
         hasrestart => true,
-        require    => Package[ ["nagios3", "nagios-nrpe-plugin", "nagios-plugins",] ],
+        require    => Package[$package_name_list ],
     }
 
     #
     # workaround for Debian packaging / Puppet design decision
     # regarding resource management in "non-standard" locations
-    file { "/etc/nagios":
-        ensure => link,
-        target => "/etc/nagios3/conf.d",
+    if $osfamily == 'debian' {
+        file { "/etc/nagios":
+            ensure => link,
+            target => "/etc/nagios3/conf.d",
+        }
     }
 
     Nagios_host <<| |>> {
-        notify => Service["nagios3"],
+        notify => Service[ $service_name ],
     }
 
     Nagios_service <<| |>> {
-        notify => Service["nagios3"],
+        notify => Service[ $service_name ],
     }
 
     Nagios_hostextinfo <<| |>>
 
-    file {[ "/etc/nagios3/conf.d/nagios_command.cfg",
-            "/etc/nagios3/conf.d/nagios_host.cfg",
-            "/etc/nagios3/conf.d/nagios_hostextinfo.cfg",
-            "/etc/nagios3/conf.d/nagios_service.cfg", ]:
+    file { $naginator::cfg_files :
         ensure  => file,
         mode    => 0644,
         owner   => root,
         group   => root,
         replace => false,
-        notify  => Service["nagios3"],
-        require => Package["nagios3"],
+        notify  => Service[$service_name],
+        require => Package[$package_name],
     }
 
-    file { "/etc/nagios3/htpasswd.users":
+    file { $naginator::nagios_users:
         ensure  => file,
         mode    => 0644,
         owner   => root,
         group   => root,
         source  => 'puppet:///modules/naginator/htpasswd.users',
-        require => Package["nagios3"],
+        require => Package[$package_name],
     }
 
-    file { "/etc/nagios3/cgi.cfg":
+    file { $naginator::cgi_cfg:
         ensure  => file,
         mode    => 0644,
         owner   => root,
         group   => root,
         source  => 'puppet:///modules/naginator/cgi.cfg',
-        require => Package["nagios3"],
+        require => Package[$package_name],
     }
 
 }
