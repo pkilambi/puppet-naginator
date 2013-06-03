18b6f9adf8b9e63bfddab5cde436ee24aa78faa1
Pradeep Kilambi <pkilambi@cisco.com>
2013-06-03 16:22:03 -0400

    more changes to get naginator working on redhat

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,51 +8,25 @@
 #
 
 class naginator {
-  case $::osfamily {
-   'redhat': {
-        $service_name      = 'nagios'
-        $package_name      = 'nagios'
-        $package_name_list = [ "nagios", "nagios-plugins-nrpe", "nagios-plugins", ]
-        $cfg_files         = [ "/etc/nagios/conf.d/nagios_command.cfg",
-                               "/etc/nagios/conf.d/nagios_host.cfg",
-                               "/etc/nagios/conf.d/nagios_hostextinfo.cfg",
-                               "/etc/nagios/conf.d/nagios_service.cfg", ]
-        $nagios_users      = '/etc/nagios/htpasswd.users'
-        $cgi_cfg           = '/etc/nagios/cgi.cfg'
-   }
-   'debian': {
-        $service_name      = 'nagios3'
-        $package_name      = 'nagios3'
-        $package_name_list = [ "nagios3", "nagios-nrpe-plugin", "nagios-plugins",
-                               "nagios3-doc", ]
-        $cfg_files         = [ "/etc/nagios3/conf.d/nagios_command.cfg",
-                               "/etc/nagios3/conf.d/nagios_host.cfg",
-                               "/etc/nagios3/conf.d/nagios_hostextinfo.cfg",
-                               "/etc/nagios3/conf.d/nagios_service.cfg", ]
-        $nagios_users      = "/etc/nagios3/htpasswd.users"
-        $cgi_cfg           = '/etc/nagios3/cgi.cfg'
-   }
-    default: {
-        fail("unsupported osfamily: $::osfamily")
-   }
- }
+ 
+  include naginator::params
 
-    package { $package_name_list:
+    package { $::naginator::params::package_name_list:
         ensure => installed,
     }
 
-    service { $naginator::service_name:
+    service { $::naginator::params::service_name:
         ensure     => running,
         enable     => true,
         hasstatus  => true,
         hasrestart => true,
-        require    => Package[$package_name_list ],
+        require    => Package[$::naginator::params::package_name_list ],
     }
 
     #
     # workaround for Debian packaging / Puppet design decision
     # regarding resource management in "non-standard" locations
-    if $osfamily == 'debian' {
+     if $osfamily == 'debian' {
         file { "/etc/nagios":
             ensure => link,
             target => "/etc/nagios3/conf.d",
@@ -60,41 +34,41 @@ class naginator {
     }
 
     Nagios_host <<| |>> {
-        notify => Service[ $service_name ],
+        notify => Service[ $::naginator::params::service_name ],
     }
 
     Nagios_service <<| |>> {
-        notify => Service[ $service_name ],
+        notify => Service[ $::naginator::params::service_name ],
     }
 
     Nagios_hostextinfo <<| |>>
 
-    file { $naginator::cfg_files :
+    file { $::naginator::params::cfg_files :
         ensure  => file,
         mode    => 0644,
         owner   => root,
         group   => root,
         replace => false,
-        notify  => Service[$service_name],
-        require => Package[$package_name],
+        notify  => Service[$::naginator::params::service_name],
+        require => Package[$::naginator::params::package_name],
     }
 
-    file { $naginator::nagios_users:
+    file { $::naginator::params::nagios_users:
         ensure  => file,
         mode    => 0644,
         owner   => root,
         group   => root,
         source  => 'puppet:///modules/naginator/htpasswd.users',
-        require => Package[$package_name],
+        require => Package[$::naginator::params::package_name],
     }
 
-    file { $naginator::cgi_cfg:
+    file { $::naginator::params::cgi_cfg:
         ensure  => file,
         mode    => 0644,
         owner   => root,
         group   => root,
         source  => 'puppet:///modules/naginator/cgi.cfg',
-        require => Package[$package_name],
+        require => Package[$::naginator::params::package_name],
     }
 
 }
--- manifests/nrpe/command.pp
+++ manifests/nrpe/command.pp
@@ -4,14 +4,16 @@
 
 define naginator::nrpe::command ( $command,) {
 
-    file { "/etc/nagios/nrpe.d/${title}.cfg":
+    include naginator::params
+
+    file { "${::naginator::params::nrpe_dir}/${title}.cfg":
         ensure => present,
         mode   => 0755,
         owner  => root,
         group  => root,
         content => template("naginator/command.erb"),
-        require => Package[ ["nagios-nrpe-server", "nagios-plugins"] ],
-        notify  => Service["nagios-nrpe-server"],
+        require => Package[ [ $::naginator::params::nrpe_package, $::naginator::params::nagios_plugin] ],
+        notify  => Service[$::naginator::params::nrpe_service],
     }
 
 }
